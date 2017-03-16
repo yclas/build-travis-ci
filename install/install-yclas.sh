e8bfc067c86ab787aede1331d0328d6a7d02b08b
kotsios5 <pantazis.constantinos@gmail.com>
2017-03-16 17:29:51 +0000

    fixes for repo

--- install/install-yclas.sh
+++ install/install-yclas.sh
@@ -4,11 +4,11 @@
 # remove install/install.lock
 # Import database with admin user and 4 ads
 
-sudo cp /var/www/yclas-test/build/install/auth.php /var/www/yclas-test/oc/config/
-sudo cp /var/www/yclas-test/build/install/database.php /var/www/yclas-test/oc/config/
-sudo cp /var/www/yclas-test/build/install/robots /var/www/yclas-test/robots.txt
-sudo cp /var/www/yclas-test/build/install/sitemap /var/www/yclas-test/sitemap.xml
-sudo cp /var/www/yclas-test/build/install/htaccess-install /var/www/yclas-test/.htaccess
-sudo rm -rf /var/www/yclas-test/install/
-sudo rm -rf /var/www/yclas-test/install-yclas.php
-mysql -uroot openclassifieds < /var/www/yclas-test/build/install/openclassifieds.sql
+sudo cp /var/www/openclassifieds2/build/install/auth.php /var/www/openclassifieds2/oc/config/
+sudo cp /var/www/openclassifieds2/build/install/database.php /var/www/openclassifieds2/oc/config/
+sudo cp /var/www/openclassifieds2/build/install/robots /var/www/openclassifieds2/robots.txt
+sudo cp /var/www/openclassifieds2/build/install/sitemap /var/www/openclassifieds2/sitemap.xml
+sudo cp /var/www/openclassifieds2/build/install/htaccess-install /var/www/openclassifieds2/.htaccess
+sudo rm -rf /var/www/openclassifieds2/install/
+sudo rm -rf /var/www/openclassifieds2/install-yclas.php
+mysql -uroot openclassifieds < /var/www/openclassifieds2/build/install/openclassifieds.sql
