7835954aeff65826fff338c26c624f493c54b013
kotsios5 <pantazis.constantinos@gmail.com>
2017-03-21 09:48:54 +0000

    changes for yclas repo on travis

--- install/install-yclas.sh
+++ install/install-yclas.sh
@@ -1,14 +1,13 @@
-# Automated installation
+# Installation for travis-ci
 
 # Move files that installation should create
-# remove install/install.lock
 # Import database with admin user and 4 ads
 
-sudo cp /var/www/openclassifieds2/build/install/auth.php /var/www/openclassifieds2/oc/config/
-sudo cp /var/www/openclassifieds2/build/install/database.php /var/www/openclassifieds2/oc/config/
-sudo cp /var/www/openclassifieds2/build/install/robots /var/www/openclassifieds2/robots.txt
-sudo cp /var/www/openclassifieds2/build/install/sitemap /var/www/openclassifieds2/sitemap.xml
-sudo cp /var/www/openclassifieds2/build/install/htaccess-install /var/www/openclassifieds2/.htaccess
-sudo rm -rf /var/www/openclassifieds2/install/
-sudo rm -rf /var/www/openclassifieds2/install-yclas.php
-mysql -uroot openclassifieds < /var/www/openclassifieds2/build/install/openclassifieds.sql
+sudo cp /var/www/yclas/build/install/auth.php /var/www/yclas/oc/config/
+sudo cp /var/www/yclas/build/install/database.php /var/www/yclas/oc/config/
+sudo cp /var/www/yclas/build/install/robots /var/www/yclas/robots.txt
+sudo cp /var/www/yclas/build/install/sitemap /var/www/yclas/sitemap.xml
+sudo cp /var/www/yclas/build/install/htaccess-install /var/www/yclas/.htaccess
+sudo rm -rf /var/www/yclas/install/
+sudo rm -rf /var/www/yclas/install-yclas.php
+mysql -uroot openclassifieds < /var/www/yclas/build/install/openclassifieds.sql
