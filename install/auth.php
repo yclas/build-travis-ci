757c782f61c9560a3e0054148c3a307e0b906b29
kotsios5 <pantazis.constantinos@gmail.com>
2017-09-11 14:28:04 +0100

    fix build

--- install/auth.php
+++ install/auth.php
@@ -4,11 +4,11 @@ return array(
 
 	'driver'       => 'oc',
 	'hash_method'  => 'sha256',
-	'hash_key'     => 'mb2c2za@escscbf*',
+	'hash_key'     => 'x5k2k2x7bpg27zqy',
 	'lifetime'     => 90*24*60*60,
 	'session_type' => Session::$default,
 	'session_key'  => 'auth_user',
-	'cookie_salt'  => 'cookie_mb2c2za@escscbf*',
+	'cookie_salt'  => 'cookie_x5k2k2x7bpg27zqy',
     'ql_lifetime'  => 7*24*60*60,
     'ql_separator' => '|',
 );
